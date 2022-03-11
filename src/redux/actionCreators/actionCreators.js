51cb2eeaa3a85dcc574bb36e7bb1e6c78da49763
acalvom <andreac.3108@gmail.com>
2022-03-11 17:06:02 +0100

    Add thunk middleware & Convert Action Creators

--- src/redux/actionCreators/actionCreators.js
+++ src/redux/actionCreators/actionCreators.js
@@ -2,11 +2,21 @@
 // Actions have two parameters: type and payload. They are dispatched by the action creators and executed by the reducers
 
 const depositMoney = (amount) => {
-    return { type: 'deposit', payload: amount }
+    return (dispatch) => {
+        dispatch({ 
+            type: 'deposit', 
+            payload: amount 
+        });
+    }
 }
 
 const withdrawMoney = (amount) => {
-    return { type: 'withdraw', payload: amount }
+    return (dispatch) => {
+        dispatch({ 
+            type: 'withdraw', 
+            payload: amount 
+        });
+    }
 }
 
 export { depositMoney, withdrawMoney }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 // This is the file where the store is created.
 // The parameters are the reducers and a default inital state (empty)
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers/rootReducers';
+import thunk from 'redux-thunk';
 
+// Thunk is a miidleware which allows redux dealing with async functions
 export const store = createStore(
     reducers,
-    {}
+    {},
+    applyMiddleware(thunk)
 );
