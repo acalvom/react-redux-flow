e6275376bfac210507b55fa232445e0d92629eac
acalvom <andreac.3108@gmail.com>
2022-03-11 10:14:29 +0100

    Create account reducer

---
+++ src/redux/reducers/accountReducer.js
@@ -0,0 +1,18 @@
+// Reducer simply returns an updated state
+// Input paramenters are the current state and the action
+
+
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case "deposit":
+            return state + action.payload;
+
+        case "withdraw":
+            return state - action.payload;
+
+        default:
+            throw new Error('Invalid Action');
+    }
+};
+
+export default reducer;
