e3faf3478b8d24d2be5c87c2d5ae1d203d8bf551
acalvom <andreac.3108@gmail.com>
2022-03-11 10:42:30 +0100

    Add store Provider to wrap up the App

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
+// The App needs to be wrapped up by a Provider to let the store access to all the components in the app
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
