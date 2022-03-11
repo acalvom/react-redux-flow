ad6f2d5332c49db5907013e487cb57ca57aa6a6f
acalvom <andreac.3108@gmail.com>
2022-03-11 14:30:05 +0100

    Add Dispatchers

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
+import { actionCreators } from './../redux/index';
 
 // To access to the data in the store in a component useSelector hook is required
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 const App = () => {
 
   // The state of the store. Returns a callback fucntion with state from all the reducers. To select a single reducers access like an object
-  const accountState = useSelector(state => state.account)
+  const accountState = useSelector(state => state.account);
+  const dispatch = useDispatch();
 
   return (
     <div className='App'>
       App Component
       <p>Store state is: {accountState} </p>
-
+      <button onClick={() => dispatch(actionCreators.depositMoney(10))}>Deposit</button>
+      <button onClick={() => dispatch(actionCreators.withdrawMoney(10))}>Withdraw</button>
     </div>
   );
 };
