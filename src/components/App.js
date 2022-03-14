84b382b2168e25199fc54fd6530b8b17c5fe92af
acalvom <andreac.3108@gmail.com>
2022-03-14 11:15:35 +0100

    Add user role state to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { actionCreators } from './../redux/index';
+import { accountCreators, usersCreators } from './../redux/index';
 
 // To access to the data in the store in a component useSelector hook is required
 import { useSelector, useDispatch } from 'react-redux';
@@ -9,14 +9,22 @@ const App = () => {
 
   // The state of the store. Returns a callback fucntion with state from all the reducers. To select a single reducers access like an object
   const accountState = useSelector(state => state.account);
+  const usersState = useSelector(state => state.users);
+
   const dispatch = useDispatch();
 
   return (
     <div className='App'>
-      App Component
-      <p>Store state is: {accountState} </p>
-      <button onClick={() => dispatch(actionCreators.depositMoney(10))}>Deposit</button>
-      <button onClick={() => dispatch(actionCreators.withdrawMoney(10))}>Withdraw</button>
+      <div>
+        <p>Store state is: {accountState} </p>
+        <button onClick={() => dispatch(accountCreators.depositMoney(10))}>Deposit</button>
+        <button onClick={() => dispatch(accountCreators.withdrawMoney(10))}>Withdraw</button>
+      </div>
+      <div>
+        <p>User role is: {usersState.role} </p>
+        <input type="checkbox" id="toggleRole" onClick={() => dispatch(usersCreators.toogleRole())} />
+        <label for="toggleRole">Set Admin</label>
+      </div>
     </div>
   );
 };
